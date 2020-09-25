d77dd00298b12e13d5e66c183d19d3be1048f248
apgit33 <adrienpaturot@yahoo.fr>
2020-09-25 17:00:43 +0200

    Update footer

--- footer.php
+++ footer.php
@@ -4,5 +4,16 @@
 // wp_footer() 
 ?>
         
+<!-- <div class="columns">
+    <div class="column">
+        <h4>Réseaux sociaux</h4>
+    </div>
+    <div class="column">
+    <h4>Informations de contact</h4>
+        
+    </div>
+</div>
+<p>Copyright - Paturot Adrien</p> -->
+    <script src="<?=PATH?>/js/main.js "></script>
     </body>
 </html>
