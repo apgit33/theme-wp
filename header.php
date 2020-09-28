169c50710aedd4923f2698e20b0dcf5b37374d24
apgit33 <adrienpaturot@yahoo.fr>
2020-09-28 17:00:09 +0200

    Update menu

--- header.php
+++ header.php
@@ -10,61 +10,33 @@
 
 <nav class="navbar" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
-        <?php 
-        // $title = explode('|', wp_get_document_title())[0];
-        // $title2 = explode('|', wp_get_document_title())[1];
-        // echo $title.$title2;
-?>
-        <a class="navbar-item" href="#">
-        
+        <a class="navbar-item" href="<?php home_url() ?>">
             <img src="<?=PATH?>/images/logo.svg" alt="logo" width="112" height="28">
-            <!-- <img src="https://bulma.io/images/bulma-logo.png" alt="logo" width="112" height="28"> -->
-        <?php // echo $title ?>
-    </a>
-        <?php
-	// } 
-  ?>
-    
-
-        <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+        </a>
+        
+        <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbartop">
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
         </a>
     </div>
 
-    <div id="navbarBasicExample" class="navbar-menu">
+    <div id="navbartop" class="navbar-menu">
         <div class="navbar-start">
-            <a class="navbar-item">
-                Home
-            </a>
-
-            <a class="navbar-item">
-                Documentation
-            </a>
-
-            <div class="navbar-item has-dropdown is-hoverable">
-                <a class="navbar-link">
-                More
-                </a>
-
-                <div class="navbar-dropdown">
-                    <a class="navbar-item">
-                        About
-                    </a>
-                    <a class="navbar-item">
-                        Jobs
-                    </a>
-                    <a class="navbar-item">
-                        Contact
-                    </a>
-                    <hr class="navbar-divider">
-                    <a class="navbar-item">
-                        Report an issue
-                    </a>
-                </div>
+            <!-- <a class="navbar-item"> -->
+            <?php
+            if( has_nav_menu('header-menu')) {
+                wp_nav_menu(
+                    array(
+                        'theme_location' => 'header-menu',
+                        'container' => false,
+                        'menu_class' => 'navbar-item'
+                    )
+                );
+            }
+        ?>
+            <!-- </a> -->
             </div>
-        </div>
 
         <div class="navbar-end">
 
@@ -80,16 +52,4 @@
     </div>
 </section>
 <button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
-<div class="container">
-    <div>
-        <?php
-            if( has_nav_menu('header-menu')) {
-                wp_nav_menu(
-                    array(
-                        'theme_location' => 'header-menu',
-                        'menu_class' => 'navbar'
-                    )
-                );
-            }
-        ?>
-    </div>
+<!-- <div class="container"> -->
--- js/main.js
+++ js/main.js
@@ -17,3 +17,23 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+//on récupère les burgers (normalement un seul)
+const burgers = document.getElementsByClassName('navbar-burger');
+
+//Pour chaque burger on ajoute un déclencheur d'évènement au click, qui toggle la classe is-active à celui-ci(this) et toggle la classe animated au menu($target)
+for (let burger of burgers) {
+    burger.addEventListener('click', function(){
+        console.log("trst");
+      //on récupère l'id cible de l'attribut "data-target" du burger
+      const target = this.dataset.target;
+      //on récupère l'élement du target
+      const $target = document.getElementById(target);
+
+      //on toggle la classe is-active au burger
+      this.classList.toggle('is-active');
+      //on toggle la classe animated au target du burger (menu)
+      $target.classList.toggle('animated');
+      $target.classList.toggle('is-active');
+  })
+}
