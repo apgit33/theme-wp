5e597fd21d341a253e3a04c45b6eb343f5b74aaf
apgit33 <adrienpaturot@yahoo.fr>
2020-09-28 17:01:23 +0200

    Add social menu

--- footer.php
+++ footer.php
@@ -1,19 +1,47 @@
-</div>
+
 
 <?php 
-// wp_footer() 
+wp_footer() 
 ?>
         
-<!-- <div class="columns">
-    <div class="column">
-        <h4>Réseaux sociaux</h4>
+    <div class="columns">
+        <div class="column">
+            <h4 class="contact has-text-centered">Informations de contact</h4>
+            
+            <p class="phone has-text-centered">06 XX XX XX XX</p>
+            <p class="mail has-text-centered">mail@mail.fr</p>
+        </div>
+
+        <div class="column">
+            <h4 class="contact has-text-centered">Réseaux sociaux</h4>
+            <div>
+            <?php
+            if( has_nav_menu('social-menu')) {
+                wp_nav_menu(
+                    array(
+                        'theme_location' => 'social-menu',
+                        'container' => false,
+                        'menu_class' => 'navbar-item'
+                    )
+                );
+            }
+        ?>
+            <i class="fa fa-star"></i>
+                <ul class="d-flex social">
+                    <li><a href="#"><span data-icon="&#xe093;"></span></a></li>
+                    <li><a href="#"><span data-icon="&#xe094;"></span></a></li>
+                    <li><a href="#"><span data-icon="&#xe097;"></span></a></li>
+                    <li><a href="#"><span data-icon="&#xe09c;"></span></a></li>
+                    <li><a href="#"><span data-icon="&#xe09d;"></span></a></li>
+                    <li><a href="#"><span data-icon="&#xe09b;"></span></a></li>
+                    <li><a href="#"><span data-icon="&#xe09a;"></span></a></li>
+                    <li><a href="#"><span data-icon="&#xe09e;"></span></a></li>
+                </ul>
+            </div>
+        </div>
     </div>
-    <div class="column">
-    <h4>Informations de contact</h4>
-        
-    </div>
-</div>
-<p>Copyright - Paturot Adrien</p> -->
+<!-- </div> -->
+<p class="has-text-centered">Copyright - Paturot Adrien</p>
     <script src="<?=PATH?>/js/main.js "></script>
     </body>
 </html>
