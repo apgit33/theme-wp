fa6906d6aede54bd49fa123fa8ab16e594d0c375
apgit33 <adrienpaturot@yahoo.fr>
2020-09-29 16:43:43 +0200

    Update 404 file & css
    1ere ébauche de la page 404

--- 404.php
+++ 404.php
@@ -1,6 +1,13 @@
 <?php get_header() ?>
 
 
-<p>Ceci est la page 404</p>
+<div class="columns">
+    <div class="column content-404">
+        <h2 class="has-text-centered">!</h2>
+        <h3 class="has-text-centered">Erreur 404</h3>
+        <p class="has-text-centered">Une erreur est parvenue, vérifiez vos branchements ou bien cliquez  sur le petit lien suivant ! <br>
+        <a href="<?php echo home_url()?>">Laissez vous guider</a></p>
+    </div>
+</div>
     
 <?php get_footer() ?>
