2fe12588d61a69289740b501c4f3d664179284dd
apgit33 <adrienpaturot@yahoo.fr>
2020-09-29 10:01:59 +0200

    Update parameters
    Ajout du téléphone

--- options/social.php
+++ options/social.php
@@ -1,17 +1,16 @@
 <?php
-
 class SocialMenu {
 
-    const GROUP = 'social_options';
+    const GROUP = "social_options";
 
     /**
-     * Ajout du menu perso au menu d'administration
+     * Ajout du menu perso au menu d"administration
      *
      * @return void
      */
     public static function register() {
-        add_action('admin_menu', [self::class, 'addMenu']);
-        add_action('admin_init', [self::class, 'registerSettings']);
+        add_action("admin_menu", [self::class, "addMenu"]);
+        add_action("admin_init", [self::class, "registerSettings"]);
     }
 
     /**
@@ -20,39 +19,61 @@ class SocialMenu {
      * @return void
      */
     public static function registerSettings() {
-        register_setting(self::GROUP, 'facebook_link', ['default' => 'salu']);
-        
-        add_settings_section('social_options_section','Paramètres',function(){
-            echo "Vous pouvez ici gérer les paramètres de vos réseaux sociaux favoris";
+        //parametres
+        register_setting(self::GROUP, "facebook_link", ["default" => "https://fr-fr.facebook.com/"]);
+        register_setting(self::GROUP, "twitter_link", ["default" => "https://twitter.com/?lang=fr"]);
+        register_setting(self::GROUP, "linkedin_link", ["default" => "https://www.linkedin.com/"]);
+        register_setting(self::GROUP, "youtube_link", ["default" => "https://www.youtube.com/"]);    
+        register_setting(self::GROUP, "email", ["default" => "contact@contact.fr"]);
+        register_setting(self::GROUP, "phone", ["default" => "06 XX XX XX XX"]);
+
+        //titre section + description
+        add_settings_section("social_options_section","Paramètres",function(){
+            echo "Vous pouvez ici gérer les paramètres des informations de contact";
         }, self::GROUP);
-        
+
+        //champ facebook
         add_settings_field("social_options_facebook", "Facebook", function() {
             ?>
-                <input name="facebook_link" type="text" value="<?php echo get_option('facebook_link') ?>">
+                <input name="facebook_link" type="text" value="<?php echo get_option("facebook_link") ?>">
             <?php
-        }, self::GROUP, 'social_options_section');
-        register_setting(self::GROUP, 'facebook_link');
+        }, self::GROUP, "social_options_section");
 
+        //champ twitter
         add_settings_field("social_options_twitter", "Twitter", function() {
             ?>
-                <input name="twitter_link" type="text" value="<?php echo get_option('twitter_link') ?>">
+                <input name="twitter_link" type="text" value="<?php echo get_option("twitter_link") ?>">
             <?php
-        }, self::GROUP, 'social_options_section');
-        register_setting(self::GROUP, 'twitter_link');
+        }, self::GROUP, "social_options_section");
         
+        //champ linkedin
         add_settings_field("social_options_linkedin", "LinkedIn", function() {
             ?>
-                <input name="linkedin_link" type="text" value="<?php echo get_option('linkedin_link') ?>">
+                <input name="linkedin_link" type="text" value="<?php echo get_option("linkedin_link") ?>">
             <?php
-        }, self::GROUP, 'social_options_section');
-        register_setting(self::GROUP, 'linkedin_link');
+        }, self::GROUP, "social_options_section");
     
+        //champ youtube
         add_settings_field("social_options_youtube", "Youtube", function() {
             ?>
-                <input name="youtube_link" type="text" value="<?php echo get_option('youtube_link') ?>">
+                <input name="youtube_link" type="text" value="<?php echo get_option("youtube_link") ?>">
             <?php
-        }, self::GROUP, 'social_options_section');
-        register_setting(self::GROUP, 'youtube_link');
+        }, self::GROUP, "social_options_section");
+
+        //champ telephone
+        add_settings_field("social_options_phone", "Téléphone", function() {
+            ?>
+                <input name="phone" type="text" value="<?php echo get_option("phone") ?>">
+            <?php
+        }, self::GROUP, "social_options_section");
+
+        //champ email
+        add_settings_field("social_options_email", "Email", function() {
+            ?>
+                <input name="email" type="email" value="<?php echo get_option("email") ?>">
+            <?php
+        }, self::GROUP, "social_options_section");
+        
     }
 
     /**
@@ -61,7 +82,7 @@ class SocialMenu {
      * @return void
      */
     public static function addMenu() {
-        add_options_page("Gestion des réseaux sociaux", "Réseaux Sociaux", "manage_options", self::GROUP, [self::class, 'render']);
+        add_options_page("Gestion des informations de contact", "Contact", "manage_options", self::GROUP, [self::class, "render"]);
     }
 
     /**
@@ -71,7 +92,7 @@ class SocialMenu {
      */
     public static function render() {
         ?>
-        <h2>Gestion des réseaux</h2>
+        <h2>Gestion des informations de contact</h2>
 
         <form action="options.php" method="post">
             <?php 
