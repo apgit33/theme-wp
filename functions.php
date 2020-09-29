6ab328a30c28cfe7662972a5846e6a22fb219735
apgit33 <adrienpaturot@yahoo.fr>
2020-09-29 16:02:33 +0200

    Restructuring

--- functions.php
+++ functions.php
@@ -1,74 +1,64 @@
 <?php
+require_once('walker/CommentsWalker.php');
+require_once("options/social.php");
 
 //ajoute une nouvelle zone de menu
 function register_menu(){
     register_nav_menus(
         array(
-            'header-menu' => __( 'Menu Header' ),
-            'social-menu' => __( 'Social Menu' )
+            "header-menu" => __( "Menu Header" ),
+            "social-menu" => __( "Social Menu" )
         )
       );
 }
-add_action('init', 'register_menu');
 
 function montheme_supports(){
-    add_theme_support('title-tag');
+    add_theme_support("title-tag");
+    add_theme_support("post--thumbnails");
 }
 
+/**
+ * Ajout des fichiers css
+ *
+ * @return void
+ */
 function montheme_register_assets(){
-    // wp_register_style('bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css');
-    // wp_enqueue_style('bootstrap');  
-    wp_register_style( 'bulma', get_template_directory_uri() . '/css/bulma.min.css' );
-    wp_enqueue_style( 'bulma' );
 
-    // wp_enqueue_style('moncss', get_stylesheet_uri());
-    wp_register_style( 'custom', get_template_directory_uri() . '/css/custom.css' );
-    wp_enqueue_style( 'custom' );
+    wp_register_style( "bulma", "https://cdn.jsdelivr.net/npm/bulma@0/css/bulma.min.css" );
+    wp_enqueue_style( "bulma" );
+
+    wp_register_style( "fontawesome", "https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5/css/all.min.css" );
+    wp_enqueue_style( "fontawesome" );
+
+    wp_register_style( "bulma", "https://cdn.jsdelivr.net/npm/bulma-social@1/bin/bulma-social.min.css" );
+    wp_enqueue_style( "bulma" );
+
+    wp_register_style( "custom", get_template_directory_uri() . "/css/custom.css" );
+    wp_enqueue_style( "custom" );
 
 }
-add_action('wp_enqueue_scripts','montheme_register_assets');
 
 function montheme_title_separator() {
-    return '|';
+    return "|";
 }
+
 function montheme_document_title_parts($title) {
     return $title;
 }
 
-
-add_action('after_setup_theme','montheme_supports');
-
-add_filter('document_title_separator', 'montheme_title_separator');
-// add_filter('document_title_parts', 'montheme_document_title_parts');
-
-
-
-// function remove_admin_login_header() {
-//     remove_action('wp_head', '_admin_bar_bump_cb');
-// }
-// add_action('get_header', 'remove_admin_login_header');
-
-// function msk_load_css_fontawesome() {
-//     if (!is_admin()) {
-//       wp_enqueue_style( 'font-awesome', '//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css', null, '4.0.1' );
-//     }
-//   }
-// add_action('wp_enqueue_scripts', 'msk_load_css_fontawesome');
-
 function montheme_menu_class($classes){
-    $classes[] = 'navbar-item';
+    $classes[] = "navbar-item";
     return $classes;
 }
 
-function montheme_menu_link_class($attrs){
-    $attrs['class'] = 'navbar-link';
-    return $attrs;
-}
-add_filter('nav_menu_css_class','montheme_menu_class');
-add_filter('nav_menu_link_attributes','montheme_menu_link_class');
+add_action("init", "register_menu");
+add_action("wp_enqueue_scripts","montheme_register_assets");
+add_action("after_setup_theme","montheme_supports");
 
 
-require_once('options/social.php');
+add_filter("document_title_separator", "montheme_title_separator");
+add_filter("document_title_parts", "montheme_document_title_parts");
+add_filter("nav_menu_css_class","montheme_menu_class");
 
 
 SocialMenu::register();
