0e81a2a85737064ebde24ce6cc22b71293749acf
apgit33 <adrienpaturot@yahoo.fr>
2020-09-29 15:51:30 +0200

    Update footer
    Ajout des réseaux sociaux et autres informations de contact

--- footer.php
+++ footer.php
@@ -4,44 +4,59 @@
 wp_footer() 
 ?>
         
-    <div class="columns">
+    <div class="columns contacts">
         <div class="column">
             <h4 class="contact has-text-centered">Informations de contact</h4>
             
-            <p class="phone has-text-centered">06 XX XX XX XX</p>
-            <p class="mail has-text-centered">mail@mail.fr</p>
+            <p class="phone has-text-centered">
+                <?php echo get_option('phone');?>
+            </p>
+            <p class="mail has-text-centered">
+                <?php echo get_option('email');?>
+            </p>
         </div>
 
         <div class="column">
             <h4 class="contact has-text-centered">Réseaux sociaux</h4>
-            <div>
-            <?php
-            if( has_nav_menu('social-menu')) {
-                wp_nav_menu(
-                    array(
-                        'theme_location' => 'social-menu',
-                        'container' => false,
-                        'menu_class' => 'navbar-item'
-                    )
-                );
-            }
-        ?>
-            <i class="fa fa-star"></i>
-                <ul class="d-flex social">
-                    <li><a href="#"><span data-icon="&#xe093;"></span></a></li>
-                    <li><a href="#"><span data-icon="&#xe094;"></span></a></li>
-                    <li><a href="#"><span data-icon="&#xe097;"></span></a></li>
-                    <li><a href="#"><span data-icon="&#xe09c;"></span></a></li>
-                    <li><a href="#"><span data-icon="&#xe09d;"></span></a></li>
-                    <li><a href="#"><span data-icon="&#xe09b;"></span></a></li>
-                    <li><a href="#"><span data-icon="&#xe09a;"></span></a></li>
-                    <li><a href="#"><span data-icon="&#xe09e;"></span></a></li>
-                </ul>
+            <div class="columns is-mobile is-centered">
+                <div class="column ">
+                    <a class="button is-medium is-facebook" href="<?php echo get_option('facebook_link') ?>">
+                        <span class="icon">
+                            <i class="fab fa-facebook fa-lg"></i>
+                        </span>
+                    </a>
+                </div>
+                <div class="column ">
+                    <a class="button is-medium is-twitter" href="<?php echo get_option('twitter_link') ?>">
+                        <span class="icon">
+                            <i class="fab fa-twitter fa-lg"></i>
+                        </span>
+                    </a>
+                </div>
+                <div class="column ">           
+                    <a class="button is-medium is-linkedin" href="<?php echo get_option('linkedin_link') ?>">
+                        <span class="icon">
+                            <i class="fab fa-linkedin fa-lg"></i>
+                        </span>
+                    </a>
+                </div>
+                <div class="column "> 
+                    <a class="button is-medium is-youtube" href="<?php echo get_option('youtube_link') ?>">
+                        <span class="icon">
+                            <i class="fab fa-youtube fa-lg"></i>
+                        </span>
+                    </a>
+                </div>
             </div>
         </div>
     </div>
-<!-- </div> -->
-<p class="has-text-centered">Copyright - Paturot Adrien</p>
+    
+    <div class="columns mb-0">
+        <div class="column mt-6">
+            <p class="has-text-centered">Copyright - Paturot Adrien</p>
+        </div>
+    </div>
+
     <script src="<?=PATH?>/js/main.js "></script>
     </body>
 </html>
